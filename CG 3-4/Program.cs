87e8a7f616dd8e811ddbbdb4fb34073805c63020
Kristen Shea <kstar61@live.com>
2018-07-24 13:59:37 -0500

    *

--- CG 3-4/Program.cs
+++ CG 3-4/Program.cs
@@ -6,7 +6,52 @@ namespace CG_3_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Basic Calculator");
+            Console.Write("Enter value for X: ");
+            string x = Console.ReadLine();
+            int xInt = int.Parse(x);
+
+            //assign string variable then convert to an integer
+
+
+            Console.Write("Enter value for Y: ");
+            string y = Console.ReadLine();
+            int yInt = int.Parse(y);
+
+            Console.Write("Would you like to add, subtract, multiply, or divide? Please choose one: ");
+            string maths = Console.ReadLine();
+
+            if (maths == "add")
+            {
+                int z = xInt + yInt;
+
+            }
+
+            else if (maths == "subtract")
+            {
+
+
+            }
+
+            else if (maths == "multiply")
+            {
+
+
+            }
+
+
+            else if (maths == "divide")
+            {
+
+
+            }
+
+
+
+
+
+
         }
     }
 }
