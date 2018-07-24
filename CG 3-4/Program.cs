aea92207bb8f51a7379ec3c344c3d5968bba3d8f
Kristen Shea <kstar61@live.com>
2018-07-24 15:46:49 -0500

    **

--- CG 3-4/Program.cs
+++ CG 3-4/Program.cs
@@ -8,46 +8,48 @@ namespace CG_3_4
         {
 
             Console.WriteLine("Basic Calculator");
+
+            //Prompt user for first value then convert from string to number, allowing for decimal places. repeated for 2nd value.
+
             Console.Write("Enter value for X: ");
             string x = Console.ReadLine();
-            int xInt = int.Parse(x);
-
-            //assign string variable then convert to an integer
-
+            double xNum = double.Parse(x);
 
             Console.Write("Enter value for Y: ");
             string y = Console.ReadLine();
-            int yInt = int.Parse(y);
+            double yNum = double.Parse(y);
+
+            //prompt user for action that should be taken with the two numbers they entered.
 
             Console.Write("Would you like to add, subtract, multiply, or divide? Please choose one: ");
             string maths = Console.ReadLine();
+        
+            //assign value for answer to calculation, allowing for decimal places
+            double solution = 0;
+            
+            //if and else if statements that calculate user-entered numbers based on the action the user instructed the program to take
 
             if (maths == "add")
-            {
-                int z = xInt + yInt;
-
-            }
+                solution = xNum + yNum;
 
             else if (maths == "subtract")
-            {
-
-
-            }
+                solution = xNum - yNum;
 
             else if (maths == "multiply")
-            {
-
-
-            }
-
+                solution = xNum * yNum;
 
             else if (maths == "divide")
-            {
+                solution = xNum / yNum;
 
+            //else statement included to give error message if user does not enter one of the four actions given.
 
-            }
+            else
+                Console.WriteLine("Invalid Entry");
 
-
+            
+            //print the answer to the console
+            Console.WriteLine("Answer: " + solution);
+            Console.ReadLine();
 
 
 
